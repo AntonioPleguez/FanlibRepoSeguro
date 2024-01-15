2bce7b119f49f76ec99643ab93f3e6105f5208a2
antonio <antonio082002@gmail.com>
2024-01-15 13:07:31 +0100

    Contenedor de carga: index, css y javascript ademas de añadir el javascript la nueva class headerautores de autores

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,31 +1,31 @@
 //añadimos un evento para que el scroll en la posicion 0 sea visible
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   let lastScrollPosition = 0;
-  const header = document.querySelector('.header');
-//luego añadimos otro evento para que cuando hagamos la accion de scrollear se esconda si 100 es menor al movimento, sino vuelve a aparecer
-  window.addEventListener('scroll', function() {
+  const header = document.querySelector(".header");
+  const headerAutores = document.querySelector(".headerautores");
+  //luego añadimos otro evento para que cuando hagamos la accion de scrollear se esconda si 100 es menor al movimento, sino vuelve a aparecer
+  window.addEventListener("scroll", function () {
     lastScrollPosition = window.scrollY;
 
-      if (100 < lastScrollPosition) {
-          
-          header.classList.add('hide');
-      } else {
-          
-          header.classList.remove('hide');
-      }
+    if (100 < lastScrollPosition) {
+      header.classList.add("hide");
+      headerAutores.classList.add("hide");
+    } else {
+      header.classList.remove("hide");
+      headerAutores.classList.remove("hide");
+
+    }
     //indicamos que cuando la ultima posicion del scroll sea 100 y el cursor del raton se coloque donde la posicion de la navbar era 0 vuelva a aparecer con un movimiento suave
-      lastScrollPosition = 100;
-      document.querySelector(".header").onmouseover = function() {
-        document.querySelector(".header").style.top = "0";
-        header.classList.remove('hide');
-        header.classList.add('reveal');
-      }
+    lastScrollPosition = 100;
+    document.querySelector(".header").onmouseover = function () {
+      document.querySelector(".header").style.top = "0";
+      header.classList.remove("hide");
+      header.classList.add("reveal");
+    };
   });
 });
-
-window.onload = function(){
-  var cont = document.getElementById('cont-carga');
-  cont.style.visibility = 'hidden'
-  cont.style.opacity = '0'
-}
-
+window.onload = function () {
+  var cont = document.getElementById("cont-carga");
+  cont.style.visibility = "hidden";
+  cont.style.opacity = "0";
+};
